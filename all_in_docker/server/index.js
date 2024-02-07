cbb8749475158e051fa1e70b7b637a0c0f42796b
biraluu <as610271@gmail.com>
2024-02-07 17:49:27 +0545

    biraluucommit

--- all_in_docker/server/index.js
+++ all_in_docker/server/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 
 const db = mysql.createPool({
   port: 3306,
-  host: "1e1f51e514ab",
+  host: "4d36c8813958",
   user: "root",
   password: "mauFJcuf5dhRMQrjj",
   database: "employeeSystem",
